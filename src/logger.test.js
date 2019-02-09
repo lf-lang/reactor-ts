aea325fe538340b1f340803c5a4a9d86982531a6
Marten Lohstroh <marten@berkeley.edu>
2019-02-08 20:12:40 -0800

    More tests.

--- src/logger.test.js
+++ src/logger.test.js
@@ -11,11 +11,35 @@ describe('logger', function () {
         
         global.console.log = jest.fn();
 
-        //global.console.log = {warn: jest.fn()}
         logger.in._value = "Hello world!";
         logger._reactions[0][1].react(); // FIXME: should not require an argument here
 
         // The first argument of the first call to the function was 'Hello world!'
         expect(console.log.mock.calls[0][0]).toBe('Hello world!');
+    });
+});
+
+describe('logger', function () {
+    it('Logging an object', function () {
+        
+        global.console.log = jest.fn();
+        let obj = {foo: "hello", bar: "world"};
+        
+        logger.in._value = obj;
+        logger._reactions[0][1].react();
+
+        expect(console.log.mock.calls[0][0]).toBe(obj);
+    });
+});
+
+describe('logger', function () {
+    it('Logging an actor', function () {
+        
+        global.console.log = jest.fn();
+        
+        logger.in._value = logger;
+        logger._reactions[0][1].react();
+
+        expect(console.log.mock.calls[0][0]).toBe(logger);
     });
 });
