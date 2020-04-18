de604c775f53f924e7644e4e39b9bc33bf680916
mew2ub <matt.weber@berkeley.edu>
2020-04-17 17:38:03 -0700

    Extended command line arguments to accept main reactor parameters as custom arguments.

--- src/core/cli.ts
+++ src/core/cli.ts
@@ -12,7 +12,7 @@ import { LogLevel } from './util';
  * Returns null if the input is malformed.
  * @param logging the raw command line argument
  */
-function loggingCLAType(logging: string): LogLevel | null {
+export function loggingCLAType(logging: string): LogLevel | null {
     if (logging in LogLevel) {
         type LevelString = keyof typeof LogLevel;
         return LogLevel[logging as LevelString];
@@ -26,7 +26,7 @@ function loggingCLAType(logging: string): LogLevel | null {
  * Returns null if the input is malformed.
  * @param logging the raw command line argument
  */
-function unitBasedTimeValueCLAType(timeout: string): TimeValue | null {
+export function unitBasedTimeValueCLAType(timeout: string): TimeValue | null {
     let duration:number;
     let units:TimeUnit;
     let wholeTimeoutPattern = /^[0-9]+\s+[a-z]+$/;
@@ -67,7 +67,7 @@ function unitBasedTimeValueCLAType(timeout: string): TimeValue | null {
  * are true, false, or absent.
  * @param logging the raw command line argument
  */
-function booleanCLAType(bool: string): boolean | null {
+export function booleanCLAType(bool: string): boolean | null {
     if (bool === "true" ) {
         return true;
     } else if (bool === "false") {
@@ -90,31 +90,34 @@ export type ProcessedCommandLineArgs = {fast: boolean| undefined,
     logging: LogLevel | undefined, help: boolean}
 
 
+export type CommandLineOptionSpec = Array<{name: string, alias?: string, 
+    type: (arg0: string) => unknown, typeLabel?: string, description: string}>;
+
 /**
  * Configuration for command line arguments.
  * If this configuration changes, the ProcessedCommandLineArgs type must
  * change too.
  */
-export const CommandLineOptionDefs = [
-    { name: 'keepalive', alias: 'k', type: booleanCLAType, typeLabel: '[true | false]',
-        description: 'Specifies whether to stop execution if there are no events to process. ' +
-            'This defaults to false, meaning that the program will stop executing when ' +
-            'there are no more events on the event queue. If you set this to true, then ' +
-            'the program will keep executing until either the timeout logical time is ' +
-            'reached or the program is externally killed. If you have physical actions, ' +
-            'it usually makes sense to set this to true.'
+export const CommandLineOptionDefs: CommandLineOptionSpec = [
+    { name: 'keepalive', alias: 'k', type: booleanCLAType, typeLabel: '{underline [true | false]}',
+    description: 'Specifies whether to stop execution if there are no events to process. ' +
+        'This defaults to false, meaning that the program will stop executing when ' +
+        'there are no more events on the event queue. If you set this to true, then ' +
+        'the program will keep executing until either the timeout logical time is ' +
+        'reached or the program is externally killed. If you have physical actions, ' +
+        'it usually makes sense to set this to true.'
     },
-    { name: 'fast', alias: 'f', type: booleanCLAType, typeLabel: '[true | false]',
+    { name: 'fast', alias: 'f', type: booleanCLAType, typeLabel: '{underline [true | false]}',
         description: 'Specifies whether to wait for physical time to match logical time. ' +
             'The default is false. If this is true, then the program will execute as fast ' +
             'as possible, letting logical time advance faster than physical time.'
     },
-    { name: 'logging', alias: 'l', type: loggingCLAType, typeLabel: '[ERROR | WARN | INFO | LOG | DEBUG]',
+    { name: 'logging', alias: 'l', type: loggingCLAType, typeLabel: '{underline [ERROR | WARN | INFO | LOG | DEBUG]}',
         description: 'The level of diagnostic messages about execution to print to the ' +
             'console. A message will print if this parameter is greater than or equal to ' +
             'the level of the message (ERROR < WARN < INFO < LOG < DEBUG).'
     },
-    { name: 'timeout', alias: 'o', type: unitBasedTimeValueCLAType, typeLabel: "'<duration> <units>'",
+    { name: 'timeout', alias: 'o', type: unitBasedTimeValueCLAType, typeLabel: "{underline '<duration> <units>'}",
         description: 'Stop execution when logical time has advanced by the specified <duration>. ' +
             'The units can be any of nsec, usec, msec, sec, minute, hour, day, week, or the plurals ' +
             'of those. For the duration and units of a timeout argument to be parsed correctly as a ' +
@@ -124,21 +127,23 @@ export const CommandLineOptionDefs = [
     { name: 'help', alias: 'h', type: Boolean, 
         description: 'Print this usage guide. The program will not execute if this flag is present.'
     }
-  ];
+];
 
 /**
  * Configuration for command line argument usage information.
+ * Note: The order of the elements in the list is important.
  */
-export const CommandLineUsageDefs = [
-{
-    header: 'Command Line Usage for TypeScript Reactors',
-    content: 'This generated program understands the following command-line arguments, ' +
-        'each of which has a short form (one character) and a long form. ' + 
-        'If provided, a command line argument will override whatever value ' +
-        'the corresponding target property had specified in the source .lf file.'
-},
-{
-    header: 'Options',
-    optionList: CommandLineOptionDefs
-}
-]
+export const CommandLineUsageDefs : Array<{header: string, content?: string, 
+    optionList?: CommandLineOptionSpec}> = [
+        {
+            header: 'Command Line Usage for TypeScript Reactors',
+            content: 'This generated program understands the following command-line arguments, ' +
+                'each of which has a short form (one character) and a long form. ' + 
+                'If provided, a command line argument will override whatever value ' +
+                'the corresponding target property had specified in the source .lf file.'
+        },
+        {
+            header: 'Options',
+            optionList: CommandLineOptionDefs
+        }
+    ];
