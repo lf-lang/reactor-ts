d495595895c30b7c0c3437df56570adaa015619f
Marten Lohstroh <marten@berkeley.edu>
2020-04-17 22:19:02 -0700

    Added pingpong benchmark.

---
+++ src/benchmarks/PingPong.ts
@@ -0,0 +1,82 @@
+import {Args, Parameter, CalleePort, CallerPort, Triggers, Timer, Reactor, App } from "./core/reactor";
+import {TimeValue} from "./core/time"
+import {Log} from "./core/util"
+
+Log.global.level = Log.levels.ERROR;
+
+export class Ping extends Reactor {
+    count: Parameter<number>;
+    client: CallerPort<number, number>;
+    constructor (parent:Reactor, count: number = 100000) {
+        super(parent);
+        this.count = new Parameter(count); // Parameter
+        this.client = new CallerPort(this);
+        this.addReaction(
+            new Triggers(this.startup),
+            new Args(this.client, this.count),
+            function (this, __client: CallerPort<number, number>, count: Parameter<number>) {
+                var startTime = this.util.getCurrentPhysicalTime();
+                var pingsLeft = count.get();
+                while (pingsLeft > 0) {
+                    //console.log("Ping!")
+                    let ret = __client.invoke(pingsLeft)
+                    if (ret)
+                        pingsLeft -= 1;       
+                }
+                let elapsedTime = this.util.getCurrentPhysicalTime().subtract(startTime);
+                console.log("Elapsed time: " + elapsedTime);
+                this.util.requestShutdown();
+            }
+        );
+    }
+}
+
+export class Pong extends Reactor {
+    server: CalleePort<number, number>
+    dummy: Timer = new Timer(this, 0, 0)
+    constructor (parent:Reactor) {
+        super(parent);
+        this.server = new CalleePort(this);
+        this.addReaction(
+            new Triggers(this.dummy),
+            new Args(this.dummy),
+            function (this) {
+                
+            }
+        );
+        this.addReaction(
+            new Triggers(this.server),
+            new Args(this.server),
+            function (this, __server: CalleePort<number, number>) {
+                //console.log("Pong!")
+                let msg = __server.get()
+                if (msg)
+                    __server.return(msg)
+            }
+        );
+        this.addReaction(
+            new Triggers(this.dummy),
+            new Args(this.dummy),
+            function (this) {
+                
+            }
+        );
+    }
+}
+
+export class PingPong extends App {
+    ping: Ping
+    pong: Pong
+    constructor (name: string, timeout: TimeValue | undefined = undefined, keepAlive: boolean = false, fast: boolean = false, success?: () => void, fail?: () => void) {
+        super(timeout, keepAlive, fast, success, fail);
+        this.ping = new Ping(this, 25000)
+        this.pong = new Pong(this)
+        this.connect2(this.ping.client, this.pong.server)
+    }
+}
+// =============== END reactor class PingPong
+
+// ************* Instance PingPong of class PingPong
+let _app = new PingPong('PingPong', undefined, false, true)
+// ************* Starting Runtime for PingPong of class PingPong
+_app._start();
