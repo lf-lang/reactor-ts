d0cde44bbefdb0fcad3da75617b51154cd49d84a
Soroush Bateni <soroosh129@gmail.com>
2020-05-13 01:19:20 -0500

    Fixed a bug with the type test. Now forces type error.

--- __tests__/types/basic-type-tests.ts
+++ __tests__/types/basic-type-tests.ts
@@ -1,7 +1,6 @@
 /* file: __tests__/types/basic-type-tests.ts */
 import { TimeValue } from '../../src/core/time';
-import { App, Parameter, Reactor, Triggers, Present, Args, State, InPort } from '../../src/core/reactor';
-import { SingleEvent } from '../../src/share/SingleEvent';
+import { App, Parameter, Reactor, Triggers, Present, Args, State, InPort, ReactionSandbox, Write, OutPort, Timer } from '../../src/core/reactor';
 
 class Logger extends Reactor {
     i = new InPort<number>(this);
@@ -12,7 +11,7 @@ class Logger extends Reactor {
 }
 
 class SETest extends App {
-    singleEvent: SingleEvent<any>;
+    singleEvent: SingleEvent;
     logger: Logger;
 
     constructor(timeout: TimeValue, success: () => void, failure: () => void) {
@@ -22,7 +21,31 @@ class SETest extends App {
         this.logger = new Logger(this, Number(4));
 
         // Connect output of singleEvent to input of logger.
+        // $ExpectError
         this._connect(this.singleEvent.o, this.logger.i);
+    }
+}
+
+function produceOutput(this: ReactionSandbox, o: Write<string>, payload: Parameter<string>) {
+    o.set(payload.get());
+        // FIXME: create a test that actually tests double sets.
+    // It's confusing to have SingleEvent be a DoubleEvent.
+    // Duplicate sets for the same port is bad form,
+    // but its worth checking that the correct value (from the last set)
+    // is delivered.
+    console.log("Writing payload to SingleEvent's output.");
+}
+
+class SingleEvent extends Reactor {
+    o: OutPort<string> = new OutPort<string>(this);
+    t1: Timer = new Timer(this, 0, 0);
+    constructor(parent: Reactor, private readonly payload: Parameter<string>) {
+        super(parent);
+        this.addReaction(
+            new Triggers(this.t1),
+            new Args(this.getWriter(this.o), this.payload),
+            produceOutput
+        );
     }
 }
 
@@ -41,6 +64,7 @@ describe('SingleEvent', () => {
         // expect(expect(seTest.logger).toBeInstanceOf(Logger));
         // $ExpectError
         seTest.canConnect(seTest.singleEvent.o, seTest.logger.i);
+        // $ExpectError
         seTest.canConnect(seTest.logger.i, seTest.singleEvent.o);
 
         seTest._start();
