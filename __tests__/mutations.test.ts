3edcb8ed3ff521eb11d63c2fcec1f924ac3eb5d4
Marten Lohstroh <marten@berkeley.edu>
2020-08-20 22:40:08 -0700

    Start with mutation tests.

---
+++ __tests__/mutations.test.ts
@@ -0,0 +1,122 @@
+import {Reactor, Reaction, Priority, App, Triggers, InPort, Args, ArgList, Startup, Shutdown, CalleePort, CallerPort, Port, Present, OutPort, Action, Timer, State} from '../src/core/reactor';
+import { UnitBasedTimeValue, TimeUnit, TimeValue, Origin } from '../src/core/time';
+import { Log, LogLevel, SortableDependencyGraph, Sortable } from '../src/core/util';
+import { doesNotMatch } from 'assert';
+import { PrintMessage } from '../src/example/generated/DistributedLogical/Distributed_dsp';
+
+class Source extends Reactor {
+
+    timer = new Timer(this, 0, new TimeValue(1000))
+    output = new OutPort<null>(this)
+    constructor(parent: Reactor) {
+        super(parent)
+        // this.addReaction(new Triggers(this.timer), new Args(this.writable(this.output)), function(this, out, state) {
+        //     out.set(null)
+        // });
+    }
+    
+}
+
+class AddOne extends Reactor {
+    input = new InPort<number[]>(this)
+    output = new OutPort<number[]>(this)
+
+    constructor(owner: Reactor) {
+        super(owner)
+        this.addMutation(new Triggers(this.input), new Args(this.input), function(this, input) {
+            let arr = input.get()
+            if (arr !== undefined) {
+                // for (let elem of arr) {
+                //     let instance = this.newChild(this, (parent:Reactor) => {new AddOne(parent)})
+                // }
+            }  
+            
+        })
+    }
+}
+
+class Print extends Reactor {
+    input = new InPort<number[]>(this)
+}
+
+/* Set a port in startup to get thing going */
+class ScatterGather extends App {
+    
+    source = new Source(this)
+
+    adder = new AddOne(this)
+
+    print = new Print(this)
+
+    constructor() {
+        super()
+        //this._connect(this.source.output, this.adder.input)
+        this._connect(this.adder.output, this.print.input)
+    }
+    
+}
+
+class ZenoClock extends Reactor {
+    tick:Timer;
+    constructor(owner: Reactor, iteration: number) {
+        super(owner, "ZenoClock(" + iteration + ")")
+        console.log("Creating ZenoClock " + iteration)
+        this.tick = new Timer(this, 0, 0)
+        this.addReaction(new Triggers(this.tick), new Args(this.tick), function(this, tick) {
+            console.log("Tick at " + this.util.getElapsedLogicalTime())
+        })
+        if (iteration < 100) {
+            this.addMutation(new Triggers(this.tick), new Args(this.tick), function(this, tick) {
+                new ZenoClock(this.getReactor(), iteration + 1)
+            })
+        } else {
+            this.util.success()
+            this.util.requestShutdown()
+        }        
+    }
+}
+
+class Zeno extends App {
+    start = new ZenoClock(this, 1)
+    constructor(timeout: TimeValue,  success: () => void, fail: () => void) {
+        super(timeout, false, false, success, fail);
+    }
+}
+
+describe("Creating reactors at runtime", function () {
+
+    jest.setTimeout(5000);
+
+    it("Reactor with periodic timer", done => {
+        Log.global.level = LogLevel.DEBUG
+
+        let app = new Zeno(new TimeValue(5),  done, () => {})
+
+        app._start();
+    });
+
+});
+
+
+// describe("Building connections at runtime", function () {
+
+//     jest.setTimeout(5000);
+
+//     it("Missed reaction deadline on InPort", done => {
+//         Log.global.level = LogLevel.WARN
+
+//         function fail() {
+//             throw new Error("Test has failed.");
+//         };
+        
+//         let app = new ScatterGather()
+
+//         // spyOn(app, '_start').and.callThrough
+
+//         // expect(() => {app._start()}).toThrowError("Deadline violation occurred!");
+
+//         /* FIXME: Deadlines are not working */
+//         app._start();
+//     });
+
+// });
