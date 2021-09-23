953cbb605c8da4ed552587d73d124be1f633d79e
Marten Lohstroh <marten@berkeley.edu>
2021-09-22 22:57:57 -0700

    Updated Sieve implementation to behave more like the one in Savina

--- src/benchmark/Sieve.ts
+++ src/benchmark/Sieve.ts
@@ -3,28 +3,24 @@ import {TimeValue, Origin} from "../core/time"
 import {Log} from "../core/util"
 
 Log.global.level = Log.levels.INFO;
-
-var primes: Array<boolean> = new Array(100000)
-
 class Ramp extends Reactor {
-    next = new Action<number>(this, Origin.logical)
-    until: Parameter<number>;
+    next: Action<number>
+    until: Parameter<number>
     value: OutPort<number> = new OutPort(this);
-    constructor (parent:Reactor, until: number = 100000) {
-        super(parent);
-        this.until = new Parameter(until);
+    constructor (parent:Reactor, until: number = 100000, period: TimeValue) {
+        super(parent)
+        this.until = new Parameter(until)
+        this.next = new Action<number>(this, Origin.logical, period)
         this.addReaction(
             new Triggers(this.startup, this.next),
             new Args(this.schedulable(this.next), this.until, this.writable(this.value)),
             function (this, next, until, value) {
                 let n = next.get()
                 if (n === undefined) {
-                    primes[2] = true
                     next.schedule(0, 2)
                 } else {
                     if (n >= until.get()) {
                         this.util.requestStop()
-                        primes.filter((v, i) => v? console.log(i) : {})
                     } else {
                         next.schedule(0, n+1)
                         value.set(n)
@@ -38,35 +34,51 @@ class Ramp extends Reactor {
 class Filter extends Reactor {
     inp: InPort<number> = new InPort(this)
     out: OutPort<number> = new OutPort(this)
-    prime: Parameter<number>
+    startPrime: Parameter<number>
+    localPrimes: State<Array<number>>
     hasSibling: State<boolean>
-    constructor (parent:Reactor, prime: number) {
+    constructor (parent:Reactor, startPrime: number, numberOfPrimes: number) {
         super(parent);
         //console.log("Created filter with prime: " + prime)
-        this.prime = new Parameter(prime);
+        this.startPrime = new Parameter(startPrime);
+        this.localPrimes = new State(new Array<number>())
         this.hasSibling = new State(false)
         this.addMutation(
             new Triggers(this.inp),
-            new Args(this.inp, this.writable(this.out), this.prime, this.hasSibling),
-            function (this, inp, out, prime, hasSibling) {
+            new Args(this.inp, this.writable(this.out), this.startPrime, this.hasSibling, this.localPrimes),
+            function (this, inp, out, prime, hasChild, localPrimes) {
                 let p = inp.get()
-                if (p !== undefined) {                
-                    let q = prime.get()
-                    if (!Number.isInteger(p / q)) {
-                        if (!hasSibling.get()) {
-                            let n = new Filter(this.getReactor(), p)
-                            //this.start(n)
-                            // console.log("CREATING...")
-                            // let x = this.create(Filter, [this.getReactor(), p])
-                            // console.log("CREATED: " + x._getFullyQualifiedName())
-                            // FIXME: weird hack. Maybe just accept writable ports as well?
-                            var port = (out as unknown as WritablePort<number>).getPort()
-                            this.connect(port, n.inp)
-                            hasSibling.set(true)
-                            primes[p] = true
-                        } else {
-                            out.set(p)
+                if (p !== undefined) {            
+                    let seen = localPrimes.get()
+                    let size = seen.length
+                    let div = false
+                    for (let q of seen) {
+                        if (Number.isInteger(p / q)) {
+                            div = true
+                            break                            
                         }
+                    }
+                    
+                    if (!div) {
+                        if (size < numberOfPrimes) {
+                            seen.push(p)
+                            console.log("Found new prime number " + p)
+                        } else {
+                            // Potential prime found.
+                            if (!hasChild.get()) {                            
+                                let n = new Filter(this.getReactor(), p, numberOfPrimes)
+                                //this.start(n)
+                                // console.log("CREATING...")
+                                // let x = this.create(Filter, [this.getReactor(), p])
+                                // console.log("CREATED: " + x._getFullyQualifiedName())
+                                // FIXME: weird hack. Maybe just accept writable ports as well?
+                                var port = (out as unknown as WritablePort<number>).getPort()
+                                this.connect(port, n.inp)
+                                hasChild.set(true)
+                            } else {
+                                out.set(p)
+                            }
+                        }  
                     }
                 }
             }
@@ -74,33 +86,16 @@ class Filter extends Reactor {
     }
 }
 
-// class Print<T extends Present> extends Reactor {
-//     inp = new InPort<T>(this)
-//     constructor(parent: Reactor) {
-//         super(parent)
-//         this.addReaction(
-//             new Triggers(this.inp),
-//             new Args(this.inp),
-//             function (this, inp) {
-//                 console.log(inp.get())
-//             }
-//         );
-//     }
-// }
-
 class Sieve extends App {
     source: Ramp
     filter: Filter
     constructor (name: string, timeout: TimeValue | undefined = undefined, keepAlive: boolean = false, fast: boolean = false, success?: () => void, fail?: () => void) {
         super(timeout, keepAlive, fast, success, fail);
-        this.source = new Ramp(this, 100000)
-        this.filter = new Filter(this, 2)
+        this.source = new Ramp(this, 100000, TimeValue.nsec(1))
+        this.filter = new Filter(this, 2, 1000)
         this._connect(this.source.value, this.filter.inp)
     }
 }
 
-// Also see "Operational Semantics of a Parallel Object-Oriented Language" by Pierre America, Jaco de Bakker, Joost N. Kok, and Jan Rutten
-
 let sieve = new Sieve('Sieve')
-// ************* Starting Runtime for PingPong of class PingPong
 sieve._start();
