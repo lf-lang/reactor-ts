8e402e8a73f95d91616eaeb0deeb79bf9f0ec8b3
Hokeun Kim <hokeunkim@berkeley.edu>
2022-01-19 17:52:03 -0800

    Add messages used in Online Facility Location benchmark.

--- src/benchmark/FacilityLocation.ts
+++ src/benchmark/FacilityLocation.ts
@@ -1,4 +1,4 @@
-import {Args, Parameter, OutPort, InPort, Triggers, Timer, Reactor, App } from "../core/reactor";
+import {Args, Parameter, OutPort, InPort, State, Triggers, Timer, Reactor, App} from "../core/reactor";
 import {TimeValue} from "../core/time"
 import {Log} from "../core/util"
 
@@ -13,8 +13,54 @@ class Point {
     }
 }
 
+abstract class Msg {}
+
+class FacilityMsg extends Msg {
+    positionRelativeToParent: number
+    depth: number
+    point: Point
+    fromChild: Boolean
+    constructor(positionRelativeToParent: number,
+        depth: number,
+        point: Point,
+        fromChild: Boolean) {
+            super()
+            this.positionRelativeToParent = positionRelativeToParent
+            this.depth = depth
+            this.point = point
+            this.fromChild = fromChild
+        }
+}
+
+class NextCustomerMsg extends Msg {}
+
+class CustomerMsg extends Msg {
+    producer: Reactor
+    point: Point
+    constructor(producer: Reactor,
+        point: Point) {
+            super()
+            this.producer = producer
+            this.point = point
+        }
+}
+
+class RequestExitMsg extends Msg {}
+
+class ConfirmExitMsg extends Msg {
+    facilities: number
+    supportCustomers: number
+    constructor(facilities: number,
+        supportCustomers: number) {
+            super()
+            this.facilities = facilities
+            this.supportCustomers = supportCustomers
+        }
+}
+
 export class Producer extends Reactor {
-    out: OutPort<Point> = new OutPort(this);
+    out: OutPort<Msg> = new OutPort(this)
+    itemsProduced: State<number> = new State(0)
     constructor (parent:Reactor) {
         super(parent)
         this.addReaction(
@@ -22,7 +68,8 @@ export class Producer extends Reactor {
             new Args(this.writable(this.out)),
             function (this, out) {
                 var startTime = this.util.getCurrentPhysicalTime()
-                out.set(new Point(12.345, 67.89))
+                // `this` is reaction, and parent is reactor containing this reaction.
+                out.set(new CustomerMsg(parent, new Point(12.345, 67.89)))
                 let elapsedTime = this.util.getCurrentPhysicalTime().subtract(startTime)
                 console.log("Elapsed time: " + elapsedTime)
             }
@@ -31,16 +78,24 @@ export class Producer extends Reactor {
 }
 
 export class Quadrant extends Reactor {
-    in: InPort<Point> = new InPort(this)
+    in: InPort<Msg> = new InPort(this)
     constructor (parent:Reactor) {
         super(parent)
         this.addReaction(
             new Triggers(this.in),
             new Args(this.in),
             function (this, __in) {
-                let v = __in.get()
-                console.log("Input x: " + v?.x)
-                console.log("Input y: " + v?.y)
+                let msg = __in.get()
+                switch (msg?.constructor) {
+                    case CustomerMsg:
+                        console.log("Received CustomerMsg.")
+                        console.log("Point x: " + (<CustomerMsg>msg).point.x)
+                        console.log("Point y: " + (<CustomerMsg>msg).point.y)
+                        break
+                    default:
+                        console.log("Error: Recieved unknown message.")
+                        break
+                }
             }
         )
     }
@@ -51,7 +106,7 @@ export class FacilityLocation extends App {
     quadrant: Quadrant
     constructor (name: string, timeout: TimeValue | undefined = undefined, keepAlive: boolean = false, fast: boolean = false, success?: () => void, fail?: () => void) {
         super(timeout, keepAlive, fast, success, fail);
-        this.producer = new Producer(this) //1000000
+        this.producer = new Producer(this)
         this.quadrant = new Quadrant(this)
         this._connect(this.producer.out, this.quadrant.in)
     }
