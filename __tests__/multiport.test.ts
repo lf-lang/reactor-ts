9b9cdead16a5fb3571b04995a23fe60826d2bee3
Marten Lohstroh <marten@berkeley.edu>
2022-02-23 23:19:24 -0800

    Lookup names for channels of multiports. Added test.

--- __tests__/multiport.test.ts
+++ __tests__/multiport.test.ts
@@ -1,58 +1,56 @@
-import { Args, Triggers, Reactor, App, InMultiPort, InPort, OutMultiPort } from "../src/core/internal";
+import { Args, Triggers, Reactor, App, InMultiPort, OutMultiPort } from "../src/core/internal";
+class TwoInTwoOut extends Reactor {
+    inp = new InMultiPort<number>(this, 2);
+    out = new OutMultiPort<number>(this, 2);
 
-//describe('Connect two multiports and check values', () => {
+    constructor(parent: Reactor) {
+        super(parent)
+        this.addReaction(
+            new Triggers(this.inp),
+            new Args(this.inp),
+            function (this, inp) {
+                console.log("Getting triggered")
+                test('check read values', () => {
+                    expect(inp.channel(0).get()).toBe(42);
+                    expect(inp.channel(1).get()).toBe(69);
+                });
+                test('print input port names', () => {
+                    expect(inp._getName()).toBe("inp");
+                    expect(inp.channel(0)._getName()).toBe("inp[0]");
+                    expect(inp.channel(1)._getName()).toBe("inp[1]");
+                });
+            }
+        );
+        this.addReaction(
+            new Triggers(this.startup),
+            new Args(this.allWritable(this.out)),
+            function (out) {
+                test('start up reaction triggered', () => {
+                    expect(true).toBe(true);
+                });
+                out.set(0, 42)
+                out.set(1, 69)
+                test('check written values', () => {
+                    expect(out.get(0)).toBe(42);
+                    expect(out.get(1)).toBe(69);
+                });
+            }
+        );
+    }
+}
 
-    class TwoInTwoOut extends Reactor {
-        inp = new InMultiPort<number>(this, 2);
-        out = new OutMultiPort<number>(this, 2);
 
-        constructor(parent: Reactor) {
-            super(parent)
-            this.addReaction(
-                new Triggers(this.inp), 
-                new Args(this.inp),
-                function (this, inp) {
-                    console.log("Getting triggered")
-                    test('check read values', () => {
-                        expect(inp.channel(0).get()).toBe(42);
-                        expect(inp.channel(1).get()).toBe(69);
-                    });
-                }
-            );
-            this.addReaction(
-                new Triggers(this.startup), 
-                new Args(this.allWritable(this.out)),
-                function (out) {
-                    test('start up reaction triggered', () => {
-                        expect(true).toBe(true);
-                    });
-                    out.set(0, 42)
-                    out.set(1, 69)
-                    test('check written values', () => {
-                        expect(out.get(0)).toBe(42);
-                        expect(out.get(1)).toBe(69);
-                    });
-                }
-            );
-        }
+class myApp extends App {
+
+    x = new TwoInTwoOut(this);
+    y = new TwoInTwoOut(this);
+
+    constructor() {
+        super();
+        this._connectMulti([this.x.out], [this.y.inp], false)
     }
 
+}
 
-    class myApp extends App {
-        
-        x = new TwoInTwoOut(this);
-        y = new TwoInTwoOut(this);
-
-        constructor() {
-            super();
-            this._connectMulti([this.x.out], [this.y.inp], false)
-            //this._connect(this.x.out.channel(0), this.y.inp.channel(0))
-            //this._connect(this.x.out.channel(1), this.y.inp.channel(1))
-        }
-
-    }
-
-    var app = new myApp();
-    app._start()
-
-//});
+var app = new myApp();
+app._start()
