99706d349b84bc77581812ee919d2c94445ff428
Kagamihara Nadeshiko <axmmisaka@users.noreply.github.com>
2023-06-01 03:06:36 -0700

    Workaround to include test and other files

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json'
+    project: ['./tsconfig.json', "./tsconfig.remaining.json"]
   },
   ignorePatterns: ['.eslintrc.js'],
   root: true,
@@ -17,5 +17,28 @@ module.exports = {
     '@typescript-eslint/semi': ['error', 'always'],
     'no-extra-semi': 'off',
     "@typescript-eslint/no-extra-semi": "error",
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        selector: 'default',
+        format: ['camelCase'],
+        leadingUnderscore: 'allowSingleOrDouble',
+        trailingUnderscore: 'allowSingleOrDouble',
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        leadingUnderscore: 'allowSingleOrDouble',
+        trailingUnderscore: 'allowSingleOrDouble',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'enumMember',
+        format: ["camelCase", "UPPER_CASE"],
+      }
+    ],
   },
 };
